8b3f7c03ff254c272632505e8c41e63ffa9d364f
TJ King <timking451@gmail.com>
2020-09-03 14:45:10 -0400

    Save and load scanned items list

--- inbound_scanning.py
+++ inbound_scanning.py
@@ -4,13 +4,24 @@ import csv
 import collections as col
 import pandas as pd
 import re
+import shelve
+import pprint
+
+def save_scans(data):
+    shelfFile = shelve.open('scanned_items')
+    shelfFile['scanned_items'] = data
+    shelfFile.close()
+
+def load_scans():
+    shelfFile = shelve.open('scanned_items')
+    return shelfFile['scanned_items']
 
 # Create and format the dataframe
 df = pd.read_csv("MasterCrossReference.txt")
 df['OrderQty'] = df['OrderQty'].astype(int)
 df = df.astype(str)
 df['Count'] = ""
-df['OK'] = ""
+df['OK'] = "---"
 df = df.sort_values(by=['Deliverable Unit'], ascending=False)
 
 # Create empty list for UPC scans
@@ -20,28 +31,36 @@ scanned_items = []
 #Most common loop is scanning items.
 #Also accepts inputs for report generation, etc.
 while True:
-    print('Please scan an item. Enter "exit" to finalize and generate report.')
+    print('Please scan an item. Enter "help" to for more options.')
     scanned = input()
     scanned = scanned.lstrip('0')
     if scanned == 'exit':
         break
+    elif scanned == 'save':
+        save_scans(scanned_items)
+    elif scanned == 'load':
+        scanned_items = load_scans()
+    elif scanned == 'help':
+        print("'save': Save the current state of the scanned items list")
+        print("'load': Load the previously saved scanned items list")
+        print("'exit': Terminte the program and generate the tote report")
     else:
         try:
             scanned_items.append(scanned)
+            #df['Count'][df['UPC'] == scanned] = scanned_items.count(scanned)
+            # c is a Counter object, counting occurences of each UPC in scanned_items
+            # it is a dictionary with the UPC as the key and the count as the value
+            c = col.Counter(scanned_items)
+            # MAP the c values onto the Count column
+            df['Count'] = df['UPC'].map(c)
+            # Reset everything to strings to help with later equivalency tests
+            df = df.astype(str)
             a = df.loc[df.index[df['UPC'] == scanned]].transpose()
-            print(a)
-            print(f"Item count: {scanned_items.count(scanned)}")
+            pprint.pprint(a)
+            #print(f"Item count: {scanned_items.count(scanned)}")
         except ValueError:
             print("That item is not expected.")
             overs.append(scanned)
-
-# c is a Counter object, counting occurences of each UPC in scanned_items
-# it is a dictionary with the UPC as the key and the count as the value
-c = col.Counter(scanned_items)
-# MAP the c values onto the Count column
-df['Count'] = df['UPC'].map(c)
-# Reset everything to strings to help with later equivalency tests
-df = df.astype(str)
 
 # This is how you iterate over a dataframe.
 # Don't forget to use the .index on your dataframe when initializing
