86aff14132b7e9510e5d5e02efa931b72ff508ec
TJ King <timking451@gmail.com>
2020-12-20 14:27:40 -0500

    Nothing significant. Just getting current before switching branches

--- inbound_scanning.py
+++ inbound_scanning.py
@@ -1,18 +1,20 @@
 # Import dependencies
-#from inbound_module import *
-import time, csv, pprint, shelve, re
+import time
+import csv
 import collections as col
 import pandas as pd
-
-# from playsound import playsound
+import re
+import shelve
+import pprint
+#from playsound import playsound
 
 # Create and format the dataframe
 df = pd.read_csv("MasterCrossReference.txt")
-df["OrderQty"] = df["OrderQty"].astype(int)
+df['OrderQty'] = df['OrderQty'].astype(int)
 df = df.astype(str)
-df["Count"] = ""
-df["OK"] = "---"
-df = df.sort_values(by=["Deliverable Unit"], ascending=False)
+df['Count'] = ""
+df['OK'] = "---"
+df = df.sort_values(by=['Deliverable Unit'], ascending=False)
 
 # Create empty list for UPC scans
 scanned_items = []
@@ -29,7 +31,7 @@ def load_scans():
 
 def tote_report(df):
     for ind in df.index:
-        if df['OrderQty'][ind] > df['Count'][ind]:
+        if df['OrderQty'][ind].astype(int) > df['Count'][ind]:
             df['OK'][ind] = "SHORT"
         elif df['OrderQty'][ind] < df['Count'][ind]:
             df['OK'][ind] = "OVER"
@@ -66,27 +68,27 @@ def opti_report(df):
 while True:
     print('Please scan an item. Enter "help" for more options.')
     scanned = input()
-    scanned = scanned.lstrip("0")
-    if scanned == "exit":
+    scanned = scanned.lstrip('0')
+    if scanned == 'exit':
         break
-    elif scanned == "load":
+    elif scanned == 'load':
         scanned_items = load_scans()
     elif scanned == 'totes':
         tote_report(df)
     elif scanned == 'optis':
         opti_report(df)
-#    elif scanned == "check":
-#        print("Please scan the item you wish to check")
-#        check = input()
-#        check = check.lstrip("0")
-#        c = col.Counter(scanned_items)
-#        # MAP the c values onto the Count column
-#        df['Count'] = df['UPC'].map(c)
-#        print("*************************************")
-#        print("Here's what I know about that item:")
-#        a = df.loc[df.index[df['UPC'] == check]].transpose()
-#        pprint.pprint(a)
-#        print("*************************************")
+    elif scanned == "check":
+        print("Please scan the item you wish to check")
+        check = input()
+        check = check.lstrip("0")
+        c = col.Counter(scanned_items)
+        # MAP the c values onto the Count column
+        df['Count'] = df['UPC'].map(c).astype(int)
+        print("*************************************")
+        print("Here's what I know about that item:")
+        a = df.loc[df.index[df['UPC'] == check]].transpose()
+        pprint.pprint(a)
+        print("*************************************")
     elif scanned == "undo":
         scanned_items.pop()
         print("Item removed")    
@@ -94,21 +96,40 @@ while True:
         print("'load': Load the previously saved scanned items list")
         print("'totes': Generate the totes report")
         print("'optis': Generate the optis report")
-#        print("'check': Check the status of an item")
+        print("'check': Check the status of an item")
         print("'undo': Remove the most recently scanned item")
         print("'exit': Exit the program")
     else:
         try:
             scanned_items.append(scanned)
-            print_count(scanned, scanned_items, df)
-            save_scans(scanned_items)
-            print("\a")
+            #df['Count'][df['UPC'] == scanned] = scanned_items.count(scanned)
+            # c is a Counter object, counting occurences of each UPC in scanned_items
+            # it is a dictionary with the UPC as the key and the count as the value
+            c = col.Counter(scanned_items)
+            # MAP the c values onto the Count column
+            df['Count'] = df['UPC'].map(c)
+            # Reset everything to strings to help with later equivalency tests
+            df = df.astype(str)
+            a = df.loc[df.index[df['UPC'] == scanned]].transpose()
+            pprint.pprint(a)
+            #print(f"Item count: {scanned_items.count(scanned)}")
+            if len(scanned_items)%5 == 0:
+                save_scans(scanned_items)
+            #playsound('beep.wav')
         except ValueError:
             print("That item is not expected.")
 
 # This is how you iterate over a dataframe.
 # Don't forget to use the .index on your dataframe when initializing
 # the for loop.
+for ind in df.index:
+    if df['OrderQty'][ind] > df['Count'][ind]:
+        df['OK'][ind] = "SHORT"
+    elif df['OrderQty'][ind] < df['Count'][ind]:
+        df['OK'][ind] = "OVER"
+    else:
+        df['OK'][ind] = "OK"
+
+#Export one big excel file with filters and formatting
 
 
-# Export one big excel file with filters and formatting
